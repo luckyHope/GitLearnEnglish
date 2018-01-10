35203530320b50bef89c75cbaa5c8281f7d43d90
Nadezhda Bazanova <bazanovanv@gmail.com>
2018-01-10 23:57:41 +0400

    Added MVVM structure

--- LearnEnglish/View/MainWindow.xaml.cs
+++ LearnEnglish/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LearnEnglish
+namespace LearnEnglish.View
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
