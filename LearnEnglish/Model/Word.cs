ec0ebc4fb3fef17657bbf481d00b3d508600b3d3
Nadezhda Bazanova <bazanovanv@gmail.com>
2018-01-12 16:29:01 +0400

    Add SQLite and edit MainWindow

---
+++ LearnEnglish/Model/Word.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnEnglish.Model
+{
+    [Table("Words")]
+    public class Word
+    {
+        [Key]
+        public int WordId { get; set; }
+        [Required]
+        [Column("EnglishWord")]
+        public string EnglishWord { get; set; }
+        [Required]
+        [Column("RussianWord")]
+        public string RussianWord { get; set; }
+
+        public Word(string englishWord, string russianWord)
+        {
+            this.EnglishWord = englishWord;
+            this.RussianWord = russianWord;
+        }
+
+    }
+}
