92c81403af84b67e844e266f396406b29638e572
Nadezhda Bazanova <bazanovanv@gmail.com>
2018-01-12 18:48:52 +0400

    Added INotifyPropertyChanged

---
+++ LearnEnglish/Models/Word.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnEnglish.Models
+{
+    public class Word : INotifyPropertyChanged
+    {
+        private int wordId;
+        public int WordId
+        {
+            get { return wordId; }
+            set
+            {
+                if (value == wordId) return;
+                WordId = value;
+                OnPropertyChanged(nameof(WordId));
+            }
+        }
+
+        private string englishWord;
+        public string EnglishWord
+        {
+            get { return englishWord; }
+            set
+            {
+                if (value == englishWord) return;
+                EnglishWord = value;
+                OnPropertyChanged(nameof(EnglishWord));
+            }
+        }
+
+        private string russianWord;
+        public string RussianWord
+        {
+            get { return russianWord; }
+            set
+            {
+                if (value == russianWord) return;
+                RussianWord = value;
+                OnPropertyChanged(nameof(RussianWord));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        //public Word(string englishWord, string russianWord)
+        //{
+        //    this.EnglishWord = englishWord;
+        //    this.RussianWord = russianWord;
+        //}
+
+    }
+}
